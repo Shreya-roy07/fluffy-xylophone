864db5a9da5478f277ec882786ff0c8e5269488d
Shreya-roy07 <141151352+Shreya-roy07@users.noreply.github.com>
2023-12-02 12:49:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 // App.js
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Certificate from './Certificate';
 
 function App() {
+   const [studentName, setStudentName] = useState('');
+  const [studentAge, setStudentAge] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setStudentName(value);
+    } else if (name === 'age') {
+      setStudentAge(value);
+    }
+  };
+
   return (
-    <div className="App">
+  <div className="App">
       <header className="App-header">
         {/* Your header content goes here */}
       </header>
-      <p className='St_name'>
-        Shreya roy
-      </p>
+      <div className="input-container">
+        <label>
+          Name:
+          <input type="text" name="name" value={studentName} onChange={handleInputChange} />
+        </label>
+        <label>
+          Age:
+          <input type="text" name="age" value={studentAge} onChange={handleInputChange} />
+        </label>
+      </div>
+      <Certificate name={studentName} age={studentAge} />
     </div>
   );
 }
