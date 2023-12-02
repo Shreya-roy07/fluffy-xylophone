dc685a6610af14115e0803eedd95bf82b58b655e
Shreya-roy07 <141151352+Shreya-roy07@users.noreply.github.com>
2023-12-02 12:59:38 +0530

    Update certificate.jsx

--- src/certificate.jsx
+++ src/certificate.jsx
@@ -1,12 +1,14 @@
+// Certificate.js
 import React from 'react';
+import './Certificate.css';
 
 const Certificate = ({ name, age }) => {
   return (
     <div className="certificate">
       <h1>Certificate of Achievement</h1>
       <div className="student-info">
-        <p>Name: {name}</p>
-        <p>Age: {age}</p>
+        <p className="name-value">{name}</p>        
+        <p className="age-value">{age}</p>
       </div>
     </div>
   );
